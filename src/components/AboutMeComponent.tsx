7ea2bd592ca9d1f1e2ab4f45d361128f662c5d5f
TreyTheLooneyCoder <roytreslll@gmail.com>
2025-05-09 09:58:29 -0700

    - 6 chars per apostrophe (&apos;)

--- src/components/AboutMeComponent.tsx
+++ src/components/AboutMeComponent.tsx
@@ -8,16 +8,16 @@ const AboutMeComponent: React.FC = () => {
       </h2>
       <div className="bg-gradient-to-br from-[#1b1f32] to-[#0e1222] border-2 border-blue-700 rounded-xl p-6 shadow-[0_0_30px_#00ccff55] max-w-4xl mx-auto text-left">
         <p className="text-lg text-blue-100 mb-4">
-          I love deep and intricately weaved storytelling. I've been a gamer nearly my whole life, and novels have always been another of my great passions. Game development always felt like the natural carrer choice, but I never thought I’d get the opportunity to code professionally.
+          I love deep and intricately weaved storytelling. I've been a gamer nearly my whole life, and novels have always been another of my great passions. Game development always felt like the natural carrer choice, but I never thought I&apos;d get the opportunity to code professionally.
         </p>
         <p className="text-blue-100 mb-4">
-          After 9 months at CodeStack Academy, I’ve developed a strong foundation in web development, design, and several programming languages. More importantly, I’ve been taught how to learn to adapt quickly to new tools and code, and how to think like a developer.
+          After 9 months at CodeStack Academy, I&apos;ve developed a strong foundation in web development, design, and several programming languages. More importantly, I&apos;ve been taught how to learn to adapt quickly to new tools and code, and how to think like a developer.
         </p>
         <p className="text-blue-100 mb-4">
           I often understand code better through gameplay analogies, mapping methods and logic to mechanics and systems. This mindset helps me quickly conceptualize the code structure and how to implement it elsewhere later.
         </p>
         <p className="text-blue-300">
-        I’m currently working on projects that combine my love for worldbuilding with programming—developing lore-rich narratives, exploring C# game logic, and scripting dynamic gameplay systems in Lua. My goal is to build unforgettable game worlds filled with meaning, mystery, and interactivity based off of stories I create.
+        I&apos;m currently working on projects that combine my love for worldbuilding with programming—developing lore-rich narratives, exploring C# game logic, and scripting dynamic gameplay systems in Lua. My goal is to build unforgettable game worlds filled with meaning, mystery, and interactivity based off of stories I create.
         </p>
       </div>
     </div>
